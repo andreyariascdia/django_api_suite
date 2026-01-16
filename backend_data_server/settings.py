29c503e32af91c7e1e9ff804cc348d7f8a27ef19
Alejandro Arias <andrauca@espol.edu.ec>
2026-01-16 02:37:43 +0000

    debug

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9(!@q=8+62xo-qa1jo5_x2c9o-%&!cp%wwu^4z160qjbqpz2ee"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = ['aarias.pythonanywhere.com','localhost','127.0.0.1']
 
